f75c95a330a447f9310bd202f9d56155d2dd4c50
Hector Salinas <hector_sag@outlook.com>
2021-02-27 10:10:57 -0600

    Code format

--- split_strings/split_strings.go
+++ split_strings/split_strings.go
@@ -7,21 +7,21 @@ import (
 )
 
 func main() {
-  str := os.Args[1]
-  charArray := strings.Split(str, "")
-  response := []string{}
-  length := len(charArray)
-  for i := 0; i < length; i += 2 {
-	var a string
-	var b string
-	if i < length && i + 1 < length {
-		a = charArray[i]
-		b = charArray[i + 1]
-	} else {
-		a = charArray[i]
-		b = "_"
+	str := os.Args[1]
+	charArray := strings.Split(str, "")
+	response := []string{}
+	length := len(charArray)
+	for i := 0; i < length; i += 2 {
+		var a string
+		var b string
+		if i < length && i+1 < length {
+			a = charArray[i]
+			b = charArray[i+1]
+		} else {
+			a = charArray[i]
+			b = "_"
+		}
+		response = append(response, fmt.Sprintf("%s%s", a, b))
 	}
-	response = append(response, fmt.Sprintf("%s%s", a, b))
-  }
-  fmt.Println(response)
+	fmt.Println(response)
 }
